1ad68b6a6ac35f8bf3c12bc997f53425f7084da9
akazadd <akazad2668@gmail.com>
2025-02-04 12:48:14 +0600

    Added UserController

---
+++ src/main/java/com/feedback/feedback_management/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.feedback.feedback_management.controller;
+
+import com.feedback.feedback_management.entity.User;
+import com.feedback.feedback_management.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        return ResponseEntity.ok(userService.registerUser(user));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable long id) {
+        return  userService.getUserById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
